ee231448849249a2c9c3432b6db08a59e3cd4aae
yeeed711 <rladpwl910@naver.com>
2022-11-21 22:16:57 +0900

    add: babel, webpack 설정 및 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,26 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
                 exclude: /node_modules/,
             },
+            {
+                test: /\.m?js$/,
+                include: [path.resolve(__dirname, 'src')],
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                },
+                            ],
+                        ],
+                        plugins: ['@babel/plugin-proposal-class-properties'],
+                    },
+                },
+            },
         ],
     },
     devtool: 'inline-source-map',
