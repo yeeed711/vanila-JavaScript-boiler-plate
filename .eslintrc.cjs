a608204536cf4e57fc756a1777f175acdb0970fd
yeeed711 <rladpwl910@naver.com>
2022-11-21 23:07:09 +0900

    add: eslint, prettier 설치 및 esm방식으로 재설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  plugin: 'import',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,64 +4,64 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpg|gif)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/images/[name][ext]',
-                },
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.m?js$/,
-                include: [path.resolve(__dirname, 'src')],
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    useBuiltIns: 'usage',
-                                    corejs: 3,
-                                },
-                            ],
-                        ],
-                        plugins: ['@babel/plugin-proposal-class-properties'],
-                    },
-                },
-            },
-        ],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        client: {
-            logging: 'none',
-        },
-    },
-    plugins: [
-        new HtmlPlugin({
-            template: './public/index.html',
-            favicon: path.resolve(__dirname, 'public/favicon.ico'),
-        }),
-        new CopyPlugin({
-            patterns: [{ from: 'public/favicon.ico' }],
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/style.[contenthash].css',
-        }),
-    ],
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.m?js$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3
+                }
+              ]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    client: {
+      logging: 'none'
+    }
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './public/index.html',
+      favicon: path.resolve(__dirname, 'public/favicon.ico')
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public/favicon.ico' }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/style.[contenthash].css'
+    })
+  ]
 };
