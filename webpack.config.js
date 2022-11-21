119cc372c7983961c0c6b35d932deec913399692
yeeed711 <rladpwl910@naver.com>
2022-11-21 23:08:50 +0900

    mod: type module 방식으로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,69 @@
-const path = require('path');
-const HtmlPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+import { resolve } from 'path';
+import HtmlPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import MiniCssExtractPlugin, {
+  loader as _loader
+} from 'mini-css-extract-plugin';
+import path from 'path';
+const __dirname = path.resolve();
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js',
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[name][ext]'
-        }
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.m?js$/,
-        include: [path.resolve(__dirname, 'src')],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  useBuiltIns: 'usage',
-                  corejs: 3
-                }
-              ]
-            ],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
+export const entry = './src/main.js';
+export const output = {
+  path: resolve(__dirname, 'dist'),
+  filename: 'js/bundle.js',
+  clean: true
+};
+export const module = {
+  rules: [
+    {
+      test: /\.(png|jpg|gif)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'assets/images/[name][ext]'
+      }
+    },
+    {
+      test: /\.s[ac]ss$/i,
+      use: [_loader, 'css-loader', 'sass-loader'],
+      exclude: /node_modules/
+    },
+    {
+      test: /\.m?js$/,
+      include: [resolve(__dirname, 'src')],
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                corejs: 3
+              }
+            ]
+          ],
+          plugins: ['@babel/plugin-proposal-class-properties']
         }
       }
-    ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    client: {
-      logging: 'none'
     }
-  },
-  plugins: [
-    new HtmlPlugin({
-      template: './public/index.html',
-      favicon: path.resolve(__dirname, 'public/favicon.ico')
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'public/favicon.ico' }]
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/css/style.[contenthash].css'
-    })
   ]
 };
+export const devtool = 'inline-source-map';
+export const devServer = {
+  client: {
+    logging: 'none'
+  }
+};
+export const plugins = [
+  new HtmlPlugin({
+    template: './public/index.html',
+    favicon: resolve(__dirname, 'public/favicon.ico')
+  }),
+  new CopyPlugin({
+    patterns: [{ from: 'public/favicon.ico' }]
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'assets/css/style.[contenthash].css'
+  })
+];
